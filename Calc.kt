525a2e7012f9ec5fd3d461a60321e0f34e3fc96f
GunterMagno <bcalejandro@iesrafaelalberti.es>
2025-03-19 21:38:11 +0100

    s

--- Calc.kt
+++ Calc.kt
@@ -33,9 +33,11 @@ fun divide(num1: Int, num2: Int): Int {
         throw IllegalArgumentException("Error: No se puede dividir por cero")
     }
     return num1 / num2
+}
 
 fun suma(num1: Int, num2: Int): Int {
     return num1 + num2
+}
 
 fun resta(num1: Int, num2: Int): Int {
     return num1 - num2
